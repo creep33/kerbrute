58b74b4e863c17afddb9106d0564252e7e91bcbf
creep33 <mariomanfer@gmail.com>
2024-10-13 01:43:21 +0200

    NTBRUTE

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/ropnop/kerbrute/cmd"
-	"github.com/ropnop/kerbrute/util"
+	"github.com/creep33/kerbrute/cmd"
+	"github.com/creep33/kerbrute/util"
 )
 
 func main() {
